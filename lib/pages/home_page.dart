db795f1c68f0d96b2231cf185af0140ff38004f5
harrybos <harry@fastforward.ai>
2020-02-02 15:23:31 -0600

    Created login and registration base with firebase

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:kronogram/services/authentication.dart';
+
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter login demo"),
+      ),
+      body: new Container(
+        child: new Text("Hello World"),
+      ),
+    );
+  }
+}
