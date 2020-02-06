15e3eb180be3e611d1caf2577c1f69e533d80e5c
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2020-02-06 16:27:02 -0600

    Add ability to set is_new_user field on database side
    Added new method to database service to unblock Harry's work

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,19 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class BaseDatabase {
   Future<DocumentSnapshot> getUserData(String userID);
-  
+
   bool isNewUser(String userID);
+  Future<void> setIsNewUser(String userID, bool status);
 }
 
 class Database implements BaseDatabase {
   final Firestore _firestore = Firestore.instance;
   final String _usersCollectionName = "users";
-  
+
   @override
   Future <DocumentSnapshot> getUserData(String userID) {
     _firestore.collection(_usersCollectionName).document(userID).get();
   }
-  
+
   @override
   bool isNewUser(String userID) {
     bool isNewUser = false;
@@ -23,4 +24,13 @@ class Database implements BaseDatabase {
     });
     return isNewUser;
   }
+
+  @override
+  Future<void> setIsNewUser(String userID, bool status) {
+    return _firestore.collection(_usersCollectionName).document(userID).setData({
+      "is_new_user" : status.toString()
+    });
+  }
+
+
 }
