b7a0a0daa800a5e9ff8fc925fe6d621daaeafca9
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2020-02-28 17:59:55 -0600

    Add helvetica

--- lib/styles/TextStyles.dart
+++ lib/styles/TextStyles.dart
@@ -4,7 +4,7 @@ import 'package:flutter/painting.dart';
 /// TextStyle for the 'message' of a Tweet.
 const defaultTweetTextStyle = const TextStyle(
   fontSize: 17.0,
-  fontFamily: 'Helvetica',
+  fontFamily: 'HelveticaNeue',
   fontWeight: FontWeight.w300,
   height: 1.1,
   letterSpacing: 0.1,
