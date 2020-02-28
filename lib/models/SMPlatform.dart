91fe53d7c18c62229cf39fd93b652728a63c50af
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2020-02-27 20:15:05 -0600

    Remove extraneous space

--- lib/models/SMPlatform.dart
+++ lib/models/SMPlatform.dart
@@ -1,4 +1,4 @@
 /// Enumerates the different platforms that Kronogram supports.
 /// This can be easily extended in the future if we ever decide to support
 /// an additional platform.
-enum SMPlatform { facebook, instagram , twitter }
+enum SMPlatform { facebook, instagram, twitter }
