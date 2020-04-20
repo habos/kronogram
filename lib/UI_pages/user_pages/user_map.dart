88973e1408b2a608946cdebe48c82deb1ef6749e
kaseynagle <knagle@crimson.ua.edu>
2020-04-20 18:31:08 -0400

    Changed map to google map, added api key, added function that will be able to plot all user locations.

--- lib/UI_pages/user_pages/user_map.dart
+++ lib/UI_pages/user_pages/user_map.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kronogram/UI_pages/values/values.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:flutter_map/flutter_map.dart';
+//import 'package:latlong/latlong.dart';
 
 
 class UserMap extends StatefulWidget{
@@ -17,6 +19,37 @@ class UserMap extends StatefulWidget{
 }
 
 class _UserMapState extends State<UserMap> {
+  //Completer<GoogleMapController> _controller = Completer();
+  //final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+/*
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final userlocations = await locations.getuserlocations();
+    setState(() {
+      _markers.clear();
+      for (final locat in userlocations.location) {
+        final marker = Marker(
+          markerId: MarkerId(location.date),
+          position: LatLng(location.lat, location.lng),
+          infoWindow: InfoWindow(
+            title: location.date,
+          ),
+        );
+        _markers[location.date] = marker;
+      }
+    });
+  }
+ */
+
 
   @override
   void initState() {
@@ -31,8 +64,14 @@ class _UserMapState extends State<UserMap> {
 
         children: <Widget> [
           Container(
-
-            child: FlutterMap(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+              ),
+            /*child: FlutterMap(
               options: new MapOptions(
                 center: new LatLng(33.209900, -87.548200),
                 zoom: 13.0,
@@ -56,20 +95,40 @@ class _UserMapState extends State<UserMap> {
                   ],
                 ),
               ],
-            ),
+            ),*/
             constraints: BoxConstraints.expand(
                 height: 400.0
             ),
           ),
+             Container(
+               child: Column(
+                 children: <Widget>[
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: <Widget>[
+                       Text( "Locations: ",
+                         textAlign: TextAlign.left,
+                         style: TextStyle(
+                             color: Color.fromARGB(255, 0, 0, 0),
+                             fontWeight: FontWeight.w600,
+                             fontSize: 20
+                         ),
+                       ),
+                       FloatingActionButton.extended(
+                         onPressed: _viewAll,
+                         label: Text('VIEW ALL'),
+                         icon: Icon(Icons.pin_drop),
+                         backgroundColor: AppColors.voidBackground6,
+                       ),
+                     ],
+                   )
 
-             Text( "Locations: ",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.w600,
-                fontSize: 20
-                ),
-              ),
+
+                 ],
+
+               )
+             )
+
             ]
         ),
 
@@ -78,8 +137,20 @@ class _UserMapState extends State<UserMap> {
 
     );
   }
+  Future<void> _viewAll() async {
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+        (CameraPosition(
+        target: LatLng(33.209900, -87.548200),
+        zoom: 13.5,))));
+  }
 }
-
+class locationsTemp{
+  locationsTemp({
+    this.date, this.lat, this.lng,});
+  final String date;
+  final double lat;
+  final double lng;
+}
 
 
 class PlaceholderWidget extends StatelessWidget {
