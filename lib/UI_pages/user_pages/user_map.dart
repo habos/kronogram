a9c18c91e0a66c8b2f053c42f423191af3dd73a6
kaseynagle <knagle@crimson.ua.edu>
2020-04-21 19:51:38 -0400

    have location pins showing up on map with dates

--- lib/UI_pages/user_pages/user_map.dart
+++ lib/UI_pages/user_pages/user_map.dart
@@ -35,6 +35,10 @@ class _UserMapState extends State<UserMap> {
   //final LatLng _center = const LatLng(45.521563, -122.677433);
   List<KronoPost> postLocations = List();
   GoogleMapController mapController;
+  bool _loadingP = true;
+  bool _loadingM = true;
+
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{}; // CLASS MEMBER, MAP OF MARKS
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
 
@@ -45,13 +49,15 @@ class _UserMapState extends State<UserMap> {
 
 
 
-  void getPosts() async{
+void getPosts() async{
 
     Map twitterUser = await db.getTwitterInfo(widget.userId);
     List<KronoTweet> tweets = await APIcaller.requestTweets(twitterUser);
     for (KronoTweet tweet in tweets) {
       if(tweet.getLocation()!=null){
         postLocations.add(tweet);
+        //initMarker(tweet, tweet.getCreationTime());
+
       }
 
     }
@@ -61,15 +67,116 @@ class _UserMapState extends State<UserMap> {
     for (KronoFacebookPost post in posts) {
       if(post.getLocation()!=null){
         postLocations.add(post);
+        //initMarker(post, post.getCreationTime());
       }
     }
 
     postLocations.sort((a, b) => a.getCreationTime().compareTo(b.getCreationTime()) * -1);
-    setState(() {
 
+/*
+      for (var i = 0; i < postLocations.length; i++) {
+        initMarker(postLocations[i], postLocations[i].getCreationTime());
+
+      _loadingM=false;
+    }
+*/
+    _loadingP=false;
+  //_loadingM=false;
+    //createMarkers();
+    setState(() {
+      _loadingP=false;
+      //_loadingM=false;
+    });
+
+  }
+
+  void createMarkers() {
+
+
+    if(postLocations.isNotEmpty) {
+      print("locationsnotempty");
+      for (var i = 0; i < postLocations.length; i++) {
+        initMarker(postLocations[i], getDateString(postLocations[i].getCreationTime()));
+      }
+      _loadingM=false;
+    }
+
+    setState(() {
+      // adding a new marker to map
+      //_loadingM=false;
     });
   }
 
+  void initMarker(post, postId) {
+    var markerIdVal = postId;
+    final MarkerId markerId = MarkerId(markerIdVal);
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng((post.getLocation()).latitude, (post.getLocation()).longitude ),
+      icon: BitmapDescriptor.defaultMarker,
+      infoWindow: InfoWindow(title: (getDateString(post.getCreationTime()))),
+    );
+    //markers.add(marker1);
+
+    setState(() {
+      markers[markerId] = marker;
+      print(markerId);
+    });
+
+  }
+
+
+  Widget progress() {
+    return new Container(
+      child: new Stack(
+        children: <Widget>[
+          new Container(
+            alignment: AlignmentDirectional.center,
+            decoration: new BoxDecoration(
+              color: Colors.white70,
+            ),
+            child: new Container(
+              decoration: new BoxDecoration(
+                  color: AppColors.voidBackground10,
+                  borderRadius: new BorderRadius.circular(10.0)
+              ),
+              width: 300.0,
+              height: 200.0,
+              alignment: AlignmentDirectional.center,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Center(
+                    child: new SizedBox(
+                      height: 50.0,
+                      width: 50.0,
+                      child: new CircularProgressIndicator(
+                        value: null,
+                        strokeWidth: 7.0,
+                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                    child: new Center(
+                      child: new Text(
+                        "loading...",
+                        style: new TextStyle(
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 /*
   final Map<String, Marker> _markers = {};
   Future<void> _onMapCreated(GoogleMapController controller) async {
@@ -96,42 +203,92 @@ class _UserMapState extends State<UserMap> {
     // TODO: implement initState
     super.initState();
     getPosts();
+    createMarkers();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: postLocations.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                        'DATE: ' + getDateString(postLocations[index].getCreationTime()) + ' : '
+    if (_loadingP) {
+      return progress();
+    }
+    else {
+      if (_loadingM) {
+        createMarkers();
+        return progress();
+      }
+      else {
+        if (postLocations.length == 0) {
+          return Container(
+            color: AppColors.primaryBackground,
+            child: Text(
+              "There are no locations available",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20
+              ),
+            ),
+          );
+        }
+        else {
+          return Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: GoogleMap(
+                    markers: Set<Marker>.of(markers.values),
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(33.209900, -87.548200),
+                      //markers[1].position,
+                      zoom: 11.0,
                     ),
-                    if (postLocations[index].getLocation()!= null)
-                      Text(
-                        //timelinePosts[index].getLocation()).toString(),
-                          ((postLocations[index].getLocation()).latitude)
-                              .toString() + ' ' +
-                              ((postLocations[index].getLocation()).longitude)
-                                  .toString()
+                    onMapCreated: (GoogleMapController controller) {
+                      mapController = controller;
+                    },
+                    //markers: _markers.values.toSet(),
+                  ),
 
+                  constraints: BoxConstraints.expand(
+                      height: 400.0
+                  ),
+                ),
+
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("Locations: ",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20
+                            ),
+                          ),
+                          FloatingActionButton.extended(
+                            onPressed: _viewAll,
+                            label: Text('VIEW ALL'),
+                            icon: Icon(Icons.pin_drop),
+                            backgroundColor: AppColors.voidBackground6,
+                          ),
+                        ],
                       ),
 
 
-                  ],
+                    ],
+                  ),
+
+
                 ),
+              ],),);
+        }
+      }
+    }
 
-
-              );
-            })
-    );
   }
-
-
   /*
 
   @override
@@ -237,10 +394,11 @@ class _UserMapState extends State<UserMap> {
   }
 
    */
+
   Future<void> _viewAll() async {
     mapController.animateCamera(CameraUpdate.newCameraPosition(
         (CameraPosition(
-        target: LatLng(33.209900, -87.548200),
+        target: LatLng((postLocations[1].getLocation()).latitude, (postLocations[1].getLocation()).longitude),
         zoom: 13.5,))));
   }
 }
