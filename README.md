d3582951b2d03903d33b07f37925ea80a80afbb4
harrybos <harry@fastforward.ai>
2020-04-23 13:10:56 -0500

    clear repo

--- README.md
+++
@@ -1,7 +0,0 @@
-# kronogram
-
-A new Flutter application.
-
-## Getting Started
-
-Flutter and Dart are required for this app.
--- lib/UI_pages/values/radii.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/rendering.dart';
-
-class Radii {
-  static const BorderRadiusGeometry k10pxRadius =
-      BorderRadius.all(Radius.circular(10));
-}
